2e118f992605a0d460e837d81d3b5d0e1ae41cdf
Elizel <elizelprocopio@gmail.com>
2017-07-03 00:08:15 -0300

    New feeds added

--- headlines.py
+++ headlines.py
@@ -6,24 +6,49 @@ app = Flask(__name__)
 
 BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
 
 @app.route("/")
-def get_news():
+@app.route("/bbc")
+def bbc():
+    """BBC Feed"""
+    return get_news("bbc")
+
+@app.route("/cnn")
+def cnn():
+    """CNN Feed"""
+    return get_news("cnn")
+
+@app.route("/iol")
+def iol():
+    """IOL Feed"""
+    return get_news("iol")
+
+@app.route("/fox")
+def fox(): 
+    """Fox Feed"""
+    return get_news("fox")
+
+def get_news(provider):
     """ Get the news from our feed as HMTL"""
 
-    feed = feedparser.parse(BBC_FEED)
-    html = get_upper_html()
+    feed = feedparser.parse(RSS_FEEDS[provider])
+    html = get_upper_html_for(provider)
     html = html + get_articles_for(feed)
     html = html + get_lower_html()
     return html
 
-
-def get_upper_html():
+ 
+def get_upper_html_for(provider):
     """Returns a string with the upper part my html page. """
 
     return """<html>
                     <body>
-                      <h1>BBC Headlines</h1>"""
+                      <h1>{0} Headlines</h1>""".format(provider.upper())
 
 
 def get_articles_for(feed):
