44e45f716c9a1349bb72d6bd5de920b386991cc0
Elizel <elizelprocopio@gmail.com>
2017-07-06 22:56:52 -0300

    Jinja Version

--- headlines.py
+++ headlines.py
@@ -1,6 +1,7 @@
 """ Flask Ninja Way. Headlines App. Flask By Example ch2  """
 import feedparser
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
@@ -9,55 +10,17 @@ BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+             'iol': 'http://www.iol.co.za/cmlink/1.640',
+             'so' : 'https://stackoverflow.com/feeds'}
 
 
 @app.route("/")
 @app.route("/<provider>")
 def get_news_from(provider="bbc"):
     """ Get the news from our feed as HMTL"""
+    feed = feedparser.parse(RSS_FEEDS[provider]) 
+    return render_template("home.html", articles=feed['entries'])
 
-    feed = feedparser.parse(RSS_FEEDS[provider])
-    html = get_upper_html_for(provider)
-    html = html + get_articles_for(feed)
-    html = html + get_lower_html()
-    return html
-
-
-def get_upper_html_for(provider):
-    """Returns a string with the upper part my html page. """
-
-    return """<html>
-                    <body>
-                      <h1>{0} Headlines</h1>""".format(provider.upper())
-
-
-def get_articles_for(feed):
-    """ Returns an HTML string with all articles separated by divs."""
-
-    articles = ""
-    for article in feed['entries']:
-        articles = articles + get_article_as_html(article)
-    return articles
-
-
-def get_article_as_html(article):
-    """Returns a single article as HTML"""
-
-    return """
-        <div>
-            <b>{0}</b> 
-            <i>{1}</i>
-            <p>{2}</p>
-        </div>
-    """.format(article.get("title"), article.get("published"), article.get("summary"))
-
-
-def get_lower_html():
-    """Return the lower part of the HTML"""
-
-    return """</body>
-                    </html> """
 
 
 if __name__ == '__main__':
