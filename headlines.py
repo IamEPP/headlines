99f306c46ade902a013eb3977993669225f99797
Elizel <elizelprocopio@gmail.com>
2017-07-08 10:58:11 -0300

    Getting the provider as request parameter

--- headlines.py
+++ headlines.py
@@ -1,6 +1,6 @@
 """ Flask Ninja Way. Headlines App. Flask By Example ch2  """
 import feedparser
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -11,16 +11,25 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640',
-             'so' : 'https://stackoverflow.com/feeds'}
+             'so': 'https://stackoverflow.com/feeds'}
 
 
 @app.route("/")
 @app.route("/<provider>")
-def get_news_from(provider="bbc"):
+def get_news_from(): 
     """ Get the news from our feed as HMTL"""
-    feed = feedparser.parse(RSS_FEEDS[provider]) 
+    provider = get_requested_provider()
+    feed = feedparser.parse(RSS_FEEDS[provider])
     return render_template("home.html", articles=feed['entries'])
 
+
+def get_requested_provider():
+    """Get the feed provider from request """
+    query = request.args.get('provider')
+    if not query or query.lower() not in RSS_FEEDS:
+        return "bbc"
+    else:
+        return query.lower()
 
 
 if __name__ == '__main__':
